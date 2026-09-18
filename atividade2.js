086cfaf052cb56a81fa40d745a7eb91506329e6d
anandavmacario <nanda10vm@gmail.com>
2026-09-18 14:43:21 -0300

    2

---
+++ atividade2.js
@@ -0,0 +1,49 @@
+//2- Number (Números)//
+//Exercício 1 - Crie dois números e exiba a soma.//
+let soma = 10 + 3;
+console.log(soma);
+
+//Exercício 2 - Calcule a subtração.//
+let subtracao = 20 - 5;
+console.log(subtracao);
+
+//Exercício 3 - Calcule a multiplicação.//
+let multiplicacao = 8 * 7;
+console.log(multiplicacao)
+
+//Exercício 4 - Calcule a divisão.//
+let divisao = 36 / 6;
+console.log(divisao);
+
+//Exercício 5 - Calcule a média de dois números.//
+let valor1 = 10;
+let valor2 = 12;
+let valor3 = 14;
+let soma2 = valor1 + valor2 + valor3;
+let media = soma2 / 3;
+console.log(media); 
+
+//Exercício 6 - Calcule o dobro de um número.//
+let valor4 = 90;
+let dobro = valor4 * 2;
+console.log(dobro);
+
+//Exercício 7 - Calcule o resto da divisão.//
+let valor5 = 30;
+let valor6 = 4;
+let sobra = valor5 % valor6;
+console.log(sobra);
+
+//Exercício 8 - Crie um preço e um desconto.//
+let preco = 100;
+let desconto = 0.5
+let precoComDesconto = preco * desconto;
+console.log(precoComDesconto)
+
+//Exercício 9 - Converta idade em meses.//
+
+
+//Exercício 10 - Calcule o quadrado de um número.//
+let valor7 = 5;
+let quadrado = valor7 * valor7;
+console.log(quadrado);
---
+++ atividade3.js
@@ -0,0 +1,30 @@
+//3- Boolean (Verdadeiro ou Falso)//
+//Exercício 1 - Crie uma variável ativo com true.//
+
+
+//Exercício 2 - Verifique se um número é maior que 10.//
+
+
+//Exercício 3 - Verifique se a idade é maior ou igual a 18.//
+
+
+//Exercício 4 - Compare dois números iguais.//
+
+
+//Exercício 5 - Compare dois números diferentes.//
+
+
+//Exercício 6 - Verifique se um número é par.//
+
+
+//Exercício 7 - Use AND (&&) em uma condição.//
+
+
+//Exercício 8 - Use OR (||) em uma condição.//
+
+
+//Exercício 9 - Negue um valor boolean.//
+
+
+//Exercício 10 - Compare texto e número.//
+
