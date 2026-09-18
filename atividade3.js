1a31a216875c5e142e593a8dc82eecb760f6a4be
anandavmacario <nanda10vm@gmail.com>
2026-09-18 16:24:12 -0300

    4

--- atividade3.js
+++ atividade3.js
@@ -26,20 +26,31 @@ console.log(valor9 >= valor10);
 let valor11 = 6;
 let valor12 = 12;
 console.log(valor11 === valor12);
-console.log(valor11 <= valo12);
+console.log(valor11 <= valor12);
 console.log(valor11 >= valor12);
 
 //Exercício 6 - Verifique se um número é par.//
-
+let valor13 = 5;
+let par = (valor13 % 2 === 0);
+console.log(par);
 
 //Exercício 7 - Use AND (&&) em uma condição.//
-
+let idade2 = 30;
+let jovem = idade2 <15 && idade> 45;
+console.log(jovem)
 
 //Exercício 8 - Use OR (||) em uma condição.//
-
+let idade3 =17;
+let verificacao2 = idade3 <21 || idade3 > 18;
+console.log(verificacao2);
 
 //Exercício 9 - Negue um valor boolean.//
-
+let num = true;
+let inverso = !num;
+console.log(inverso);
 
 //Exercício 10 - Compare texto e número.//
-
+let texto = "17";
+let idade4 = 17;
+let comparacao1 = texto == idade;
+console.log(comparacao1)
