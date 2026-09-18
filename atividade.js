b2c70905bd0efbf26ef85479b14c337456a960d8
anandavmacario <nanda10vm@gmail.com>
2026-09-18 14:08:00 -0300

    1

---
+++ atividade.js
@@ -0,0 +1,46 @@
+//1 String Textos
+//Exercício 1 - Crie uma variável nome com seu nome e exiba no console.//
+let nome = "Ananda";
+console.log(nome);
+
+//Exercício 2 - Crie uma variável sobrenome e mostre nome + sobrenome.//
+let sobrenome = "Ventura";
+let nomeCompleto = nome + " " + sobrenome;
+console.log(sobrenome);
+console.log(nomeCompleto);
+
+//Exercício 3 - Crie uma frase usando Template String.//
+let nome1 = "Nanda";
+let frase = `Me chame de ${nome1}, por favor!`;
+console.log(frase); 
+
+//Exercício 4 - Guarde o nome de uma escola e exiba.//
+let nomeEscola = "Sesi";
+console.log(nomeEscola);
+
+//Exercício 5 - Crie duas strings e junte em uma frase.//
+let anime = "Jujutsu Kaisen";
+let personagem = "Geto";
+let frase1 = "Meu personagem favorito de " + anime + " é o " + personagem;
+console.log(frase1);
+
+//Exercício 6 - Exiba uma mensagem de boas-vindas.//
+let mensagem = "Seja bem-vindo(a) ao meu site!";
+console.log(mensagem);
+
+//Exercício 7 - Mostre o tamanho de uma string.//
+let tamanho = "da string";
+console.log(tamanho.length);
+
+//Exercício 8 - Transforme um texto em letras maiúsculas.//
+let maiusculo = "eu amo resident evil";
+console.log(maiusculo.toUpperCase());
+
+//Exercício 9 - Transforme um texto em letras minúsculas.//
+let minusculo = "eu gosto de uva";
+console.log(minusculo.toLowerCase());
+
+//Exercício 10 - Crie uma frase com nome e cidade.//
+let cidade = "Guarulhos";
+let frase2 = nome + " mora em " + cidade;
+console.log(frase2);
